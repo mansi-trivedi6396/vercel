37a4cb2e0179097797cec19c13df3ebc9a2cf8b7
mansi <mansitrivedi315@gmail.com>
2022-08-12 12:57:13 +0530

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,15 +22,13 @@
 // }
 import React from 'react'
 import Appbar from "./Admin/Component/Appbar"
-import Footer from './Admin/Component/Footer'
-import Home from './Admin/Component/Home'
+
 export default function index() {
   return (
     <>
     <Appbar/>
     
-    <Home/>
-    <Footer/>
+    
     </>
   )
 }
