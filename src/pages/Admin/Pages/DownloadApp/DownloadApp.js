ff3c24a3d548c47cb5592d6f85caae06d8a51907
mansi <mansitrivedi315@gmail.com>
2022-08-17 16:27:55 +0530

    add img name

--- src/pages/Admin/Pages/DownloadApp/DownloadApp.js
+++ src/pages/Admin/Pages/DownloadApp/DownloadApp.js
@@ -3,7 +3,7 @@ import AppBarNew from '../../Component/Appbar'
 import Footer from '../../Component/Footer'
 import img1 from '../../../../images/Main/downloadapp image.png';
 import playstore from '../../../../images/Playstore/download-on-the-app-store-apple-logo-svgrepo-com.svg'
-import Logo from '../../../../images/LOGO/Group 2639.svg'
+import Logo from '../../../../images/logo/Group 2639.svg'
 import './DownloadApp.css'
 
 function DownloadApp() {
