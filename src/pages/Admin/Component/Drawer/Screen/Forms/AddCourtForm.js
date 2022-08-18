a7ca12536d0b7d555ac53dfc11da0265fd57b74a
mansi <mansitrivedi315@gmail.com>
2022-08-18 14:23:33 +0530

    edit court form

--- src/pages/Admin/Component/Drawer/Screen/Forms/AddCourtForm.js
+++ src/pages/Admin/Component/Drawer/Screen/Forms/AddCourtForm.js
@@ -255,9 +255,76 @@
 
 // export default AddCourtForm
 import React from 'react'
-
+import {
+  Box,
+  Container,
+  FormControl,
+  Grid,
+  makeStyles,
+  Typography,
+} from "@material-ui/core"
+const useStyles = makeStyles(theme => ({
+  root: {
+    backgroundColor: "transprent",
+    width: "100%",
+    borderRadius: "10px 10px 0px 0px",
+    height: "100%",
+    paddingTop: "10px",
+    paddingLeft: "21px",
+    fontFamily: "DM Sans",
+  },
+  titleText: {
+    fontSize: "28px",
+    marginBottom: "40px",
+    fontWeight: "400",
+    fontStyle: "normal",
+  },
+  boxRoot: {
+    width: "100%",
+  },
+  profileStyle: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    color: "#B7B7BF",
+    fontSize: "20",
+    margin: "10px",
+  },
+  logoStyle: {
+    marginBottom: "10px",
+  },
+  flagSelect: {
+    background: "white",
+    border: "none",
+    borderRadius: 10,
+    position: "relative",
+    display: "flex",
+    padding: "12px",
+  },
+  flagRoot: {},
+  DivSelect: {
+    display: "flex",
+  },
+  DivSelect2: {
+    display: "flex",
+  },
+  DivSelect3: {
+    display: "flex",
+  },
+  selectIcon:{
+    "&>div .css-b62m3t-container":{
+        paddingRight:'56px !important',
+        marginTop:'11px'
+    }
+  }
+}))
 export default function AddCourtForm() {
+  const classes = useStyles()
   return (
-    <div>AddCourtForm</div>
+    <div className={classes.root}>
+       <Container maxWidth="lg" style={{marginLeft:22}}>
+       <Typography className={classes.titleText}>2. Add Courts</Typography>
+       </Container>
+    </div>
   )
 }
