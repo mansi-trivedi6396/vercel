a6b08f1cc38c6f96c7581a51932f3551539fa958
mansi <mansitrivedi315@gmail.com>
2022-08-18 16:52:31 +0530

    edit court form

--- src/pages/Admin/Component/Drawer/Screen/Forms/AddCourtForm.js
+++ src/pages/Admin/Component/Drawer/Screen/Forms/AddCourtForm.js
@@ -276,63 +276,9 @@ import NormalSelect from "../../../SelectWithImage/NormalSelect"
 import clubInfoImg from "../../../../../../images/clubInfoimg.svg"
 import SelectWithIcon from "../../../SelectWithImage/SelectWithIcon"
 import AddCourtAccordian from "../../../AddCourtAccordian"
+import useStyles from "./Styles"
 
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    backgroundColor: "transprent",
-    width: "100%",
-    borderRadius: "10px 10px 0px 0px",
-    height: "100%",
-    paddingTop: "10px",
-    paddingLeft: "21px",
-    fontFamily: "DM Sans",
-  },
-  titleText: {
-    fontSize: "28px",
-    marginBottom: "40px",
-    fontWeight: "400",
-    fontStyle: "normal",
-  },
-  boxRoot: {
-    width: "100%",
-  },
-  profileStyle: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    color: "#B7B7BF",
-    fontSize: "20",
-    margin: "10px",
-  },
-  logoStyle: {
-    marginBottom: "10px",
-  },
-  flagSelect: {
-    background: "white",
-    border: "none",
-    borderRadius: 10,
-    position: "relative",
-    display: "flex",
-    padding: "12px",
-  },
-  flagRoot: {},
-  DivSelect: {
-    display: "flex",
-  },
-  DivSelect2: {
-    display: "flex",
-  },
-  DivSelect3: {
-    display: "flex",
-  },
-  selectIcon:{
-    "&>div .css-b62m3t-container":{
-        paddingRight:'56px !important',
-        marginTop:'11px'
-    }
-  }
-}))
 
 export default function AddCourtForm() {
   const classes = useStyles()
